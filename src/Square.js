c6a14f5ad8ca5768c819f3a913b14561b11fa695
Muyinda ROgers <moverr@gmail.com>
2020-04-15 16:46:36 +0300

    Initial Game setup :

---
+++ src/Square.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Square extends Component {
+    render() {
+        return (
+            <div>
+                <button className="square">
+                    {this.props.value}
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Square;
